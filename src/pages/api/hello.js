3c279dc55ce82abdd6d16d8c21e70f909fa944c8
Pervin Çalışkan <pervincaliskan01@hotmail.com>
2023-01-24 23:10:10 +0300

    updated

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req.headers['host']);
+  // const orıgın = new URL(req.url).origin
+  res.status(200).json({ name: req.headers['host'] })
 }

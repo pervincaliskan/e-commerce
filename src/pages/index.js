055a493cce8fac0ca4df309de8992c1a633ec0da
Pervin Çalışkan <pervincaliskan01@hotmail.com>
2023-01-24 23:10:00 +0300

    index.js updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,23 @@
 import axios from 'axios';
-import { useRouter } from 'next/router';
-import React, { useContext, useEffect, useState } from 'react';
+import {useRouter} from 'next/router';
+import React, {useContext, useEffect, useState} from 'react';
 import Layout from '../components/Layout';
-import Product from '../components/Product';
-import { ProductsContext } from '../state/ProductsContext';
-
-const API_URL = `http://localhost:3000`;
+import {ProductsContext} from '../state/ProductsContext';
+import ProductList from "../components/Product/product-list";
 
 const Home = () => {
-  const { products, loading } = useContext(ProductsContext);
+    const {products, loading} = useContext(ProductsContext);
 
-  return (
-    <Layout>
-      <main>
-        <section className="grid grid-cols-3 gap-6 p-4 my-4 lg:grid-cols-4">
-          {loading && <p>Loading...</p>}
-          {products &&
-            products.length > 0 &&
-            products.map((product) => (
-              <Product key={product.id} {...product} />
-            ))}
-        </section>
-      </main>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <main>
+                {loading && <p>Loading...</p>}
+                {products &&
+                    products.length > 0 &&
+                    <ProductList title={"Oyunlar"} products={products}/>}
+            </main>
+        </Layout>
+    );
 };
 
 export default Home;

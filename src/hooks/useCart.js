b9503b48d7ea4cfecf6de3473c124d01a1b6cd56
Pervin Çalışkan <pervincaliskan01@hotmail.com>
2023-01-24 23:10:22 +0300

    useCart.js updated

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,99 +1,111 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import toast from 'react-hot-toast';
-import { supabase } from '../utils/supabaseClient';
+import {supabase} from '../utils/supabaseClient';
 
 const useCart = () => {
-  const [cartState, setCartState] = useState({});
+    const [cartState, setCartState] = useState({});
 
-  const fetchCurrentCart = async () => {
-    const { data: cart, error: cartError } = await supabase
-      .from('cart')
-      .select('*')
-      .eq('user_id', supabase.auth.session()?.user.id);
-    if (cartError) {
-      toast.error(cartError.message);
-      return;
-    }
-    setCartState(
-      cart.reduce(
-        (prev, curr) => ({
-          ...prev,
-          [curr.product_id]: curr.quantity,
-        }),
-        {}
-      )
-    );
-  };
+    const fetchCurrentCart = async () => {
 
-  const addToCart = async (product) => {
-    let error;
-    if (cartState[product.id]) {
-      const { error: updateError } = await supabase
-        .from('cart')
-        .update({
-          quantity: cartState[product.id] + 1,
-        })
-        .eq('user_id', supabase.auth.session().user.id)
-        .eq('product_id', product.id);
-      error = updateError;
-    } else {
-      const { error: insertError } = await supabase.from('cart').insert([
-        {
-          product_id: product.id,
-          quantity: cartState[product.id] || 0 + 1,
-          user_id: supabase.auth.session().user.id,
-        },
-      ]);
-      error = insertError;
-    }
-    if (error) {
-      return toast.error(error.message);
-    }
-    await fetchCurrentCart();
-  };
+        if (!supabase.auth.session()) {
+            return;
+        }
 
-  const removeFromCart = async (product) => {
-    let error;
-    if (cartState[product.id] > 1) {
-      const { error: updateError } = await supabase
-        .from('cart')
-        .update({
-          quantity: cartState[product.id] - 1,
-        })
-        .eq('product_id', product.id)
-        .eq('user_id', supabase.auth.session().user.id);
-      error = updateError;
-    } else {
-      const { error: deleteError } = await supabase
-        .from('cart')
-        .delete()
-        .eq('product_id', product.id)
-        .eq('user_id', supabase.auth.session().user.id);
-      error = deleteError;
-    }
-    if (error) {
-      return toast.error(error.message);
-    }
-    await fetchCurrentCart();
-  };
+        const {data: cart, error: cartError} = await supabase
+            .from('cart')
+            .select('*')
+            .eq('user_id', supabase.auth.session()?.user.id);
+        if (cartError) {
+            toast.error(cartError.message);
+            return;
+        }
+        setCartState(
+            cart.reduce(
+                (prev, curr) => ({
+                    ...prev,
+                    [curr.product_id]: curr.quantity,
+                }),
+                {}
+            )
+        );
+    };
 
-  const clearCart = async () => {
-    const { error } = await supabase
-      .from('cart')
-      .delete()
-      .eq('user_id', supabase.auth.session().user.id);
-    setCartState({});
-    if (error) return toast.error(error.message);
-  };
+    const addToCart = async (product) => {
+        let error;
+        if (cartState[product.id]) {
+            const {error: updateError} = await supabase
+                .from('cart')
+                .update({
+                    quantity: cartState[product.id] + 1,
+                })
+                .eq('user_id', supabase.auth.session().user.id)
+                .eq('product_id', product.id);
+            error = updateError;
+        } else {
 
-  return {
-    setCartState,
-    cartState,
-    clearCart,
-    addToCart,
-    removeFromCart,
-    fetchCurrentCart,
-  };
+            if (!supabase.auth.session()) {
+                return toast.error('Lütfen giriş yapınız!');
+            }
+
+            const {error: insertError} = await supabase.from('cart').insert([
+                {
+                    product_id: product.id,
+                    quantity: cartState[product.id] || 0 + 1,
+                    user_id: supabase.auth.session().user.id,
+                },
+            ]);
+            error = insertError;
+        }
+
+        if (error) {
+            return toast.error(error.message);
+        }
+
+        await fetchCurrentCart();
+    };
+
+    const removeFromCart = async (product) => {
+        let error;
+        if (cartState[product.id] > 1) {
+            const {error: updateError} = await supabase
+                .from('cart')
+                .update({
+                    quantity: cartState[product.id] - 1,
+                })
+                .eq('product_id', product.id)
+                .eq('user_id', supabase.auth.session().user.id);
+            error = updateError;
+        } else {
+            const {error: deleteError} = await supabase
+                .from('cart')
+                .delete()
+                .eq('product_id', product.id)
+                .eq('user_id', supabase.auth.session().user.id);
+            error = deleteError;
+        }
+        if (error) {
+            return toast.error(error.message);
+        }
+        await fetchCurrentCart();
+    };
+
+    const clearCart = async () => {
+        const {error} = await supabase
+            .from('cart')
+            .delete()
+            .eq('user_id', supabase.auth.session().user.id);
+        setCartState({});
+        if (error) return toast.error(error.message);
+    };
+
+    return {
+        setCartState,
+        cartState,
+        clearCart,
+        addToCart,
+        removeFromCart,
+        fetchCurrentCart,
+    };
 };
 
 export default useCart;
